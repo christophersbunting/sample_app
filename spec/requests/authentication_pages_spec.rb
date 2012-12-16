975d7932cfa472a02e61b606c6767cdb9d3cdd04
Chris Bunting <chris@homerun.com>
2012-12-15 23:21:22 -0800

    Creating miniposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -102,6 +102,19 @@ describe "Authentication" do
           it { should have_selector('title', text: 'Sign in') }
         end
       end
+
+      describe "in the Microposts controller" do
+
+        describe "submitting the create action" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete microposts_path(FactoryGirl.create(:microposts)) }
+          specify { response.should redirect_to(signin_path)) }
+        end
+      end
     end
 
     describe "as wrong user" do
